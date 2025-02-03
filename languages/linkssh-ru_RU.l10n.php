bd75188c2a6956ba1b1dcf2ae4701440583293da
makaravich <d.makarski@gmail.com>
2025-02-03 14:52:17 +0100

    Add languages support

---
+++ languages/linkssh-ru_RU.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'Links shortener','report-msgid-bugs-to'=>'','pot-creation-date'=>'2025-02-02 11:21+0000','po-revision-date'=>'2025-02-02 11:22+0000','last-translator'=>'','language-team'=>'Russian','language'=>'ru_RU','plural-forms'=>'nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10 >= 2 && n%10<=4 &&(n%100<10||n%100 >= 20)? 1 : 2);','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.7.1; wp-6.7.1','x-domain'=>'linkssh','messages'=>[]];
