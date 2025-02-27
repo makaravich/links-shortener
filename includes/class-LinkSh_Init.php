1e94e08c437531306c218792b89242c011778bac
makaravich <d.makarski@gmail.com>
2025-02-27 18:34:59 +0100

    Add Short Links count column to user's table

--- includes/class-LinkSh_Init.php
+++ includes/class-LinkSh_Init.php
@@ -7,6 +7,10 @@ class LinkSh_Init {
 	public function __construct() {
 		// Add styles and scripts on admin side
 		add_action( 'admin_enqueue_scripts', [ $this, 'add_admin_assets' ] );
+
+		// Add custom column to User's table
+		add_filter( 'manage_users_columns', [ $this, 'custom_add_user_columns' ] );
+		add_action( 'manage_users_custom_column', [ $this, 'custom_show_user_posts' ], 10, 3 );
 	}
 
 	/**
@@ -25,6 +29,36 @@ class LinkSh_Init {
 				'wp-util'
 			], filemtime( LINKSH_PLUGIN_BASEPATH . '/assets/js/admin.js' ) );
 		}
+	}
+
+	/**
+	 * Adds column with short links count (Title)
+	 *
+	 * @param $columns
+	 *
+	 * @return mixed
+	 */
+	public function custom_add_user_columns( $columns ): mixed {
+		$columns['shlinks_count'] = __( 'Short Links', 'links-shortener' );
+
+		return $columns;
+	}
+
+	/**
+	 * Adds column with short links count (Values)
+	 *
+	 * @param $value
+	 * @param $column_name
+	 * @param $user_id
+	 *
+	 * @return mixed|string
+	 */
+	function custom_show_user_posts( $value, $column_name, $user_id ): mixed {
+		if ( $column_name === 'shlinks_count' ) {
+			return count_user_posts( $user_id, LINKSH_POST_TYPE );
+		}
+
+		return $value;
 	}
 }
 
